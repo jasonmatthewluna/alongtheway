8c9d0af9559e15bd5b817aca90374a5f1bb0ad13
jasonmatthewluna <35713810+jasonmatthewluna@users.noreply.github.com>
2021-03-12 17:55:47 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -449,72 +449,9 @@ function get_journal_entry(scene_question){
 // 1. Getting song
 
 function get_fav_song(){
-    <!-- var fav_song_id = document.getElementById("fav_song")
-    console.log("fav song: " + fav_song_id.value); -->
- 
-    <!-- player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: 'M7lc1UVf-VE',
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  }); -->
-        
+    var fav_song_id = document.getElementById("fav_song")
+    console.log("fav song: " + fav_song_id.value);  
 }
-
-<--
-var player;
-
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
--->
-    
-<--
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: 'M7lc1UVf-VE',
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-function stopVideo() {
-  player.stopVideo();
-}
--->
-
 
 var umbrella_id = document.getElementById("umbrella")
 var umbrella_selected = false;
